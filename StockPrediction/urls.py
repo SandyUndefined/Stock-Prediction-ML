5082dce4e18306c3158c47c254d642d8500b0c1c
Sandy_undefined <08sandysk@gmail.com>
2020-06-12 19:52:39 +0530

    graphs left

--- StockPrediction/urls.py
+++ StockPrediction/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='Stock Prediction'),
+    path("", views.greet, name='greet'),
+    path("index.html", views.index, name='Stock Prediction'),
     path("Prediction/", views.prediction, name='Prediction')
 ]
--- StockPrediction/views.py
+++ StockPrediction/views.py
@@ -1,6 +1,6 @@
 import io
 import pandas as pd
-import requests
+import matplotlib.pyplot as plt
 from django.shortcuts import render
 from nsetools import Nse
 from datetime import date
@@ -9,39 +9,31 @@ from StockPrediction.stock_prediction import get_stock_data
 nse = Nse()
 
 
+def greet(request):
+    return render(request, 'StockPrediction/greet.html',)
+
+
 def index(request):
     global data
-    api_key = '1KVQW60J7LHPS12D'
-    base_url = 'https://www.alphavantage.co/query?'
-    function = 'TIME_SERIES_DAILY_ADJUSTED'
 
-    url50 = 'https://archives.nseindia.com/content/indices/ind_nifty50list.csv'
-    url100 = 'https://archives.nseindia.com/content/indices/ind_nifty100list.csv'
-    url200 = 'https://archives.nseindia.com/content/indices/ind_nifty200list.csv'
-    url500 = 'https://archives.nseindia.com/content/indices/ind_nifty500list.csv'
+    # url50 = 'https://archives.nseindia.com/content/indices/ind_nifty50list.csv'
+    # url100 = 'https://archives.nseindia.com/content/indices/ind_nifty100list.csv'
+    #
+    # sfifty = requests.get(url50).content
+    # shundred = requests.get(url100).content
 
-    sfifty = requests.get(url50).content
-    shundred = requests.get(url100).content
-    stwohundred = requests.get(url200).content
-    sfivehundred = requests.get(url500).content
-
-    nifty50 = pd.read_csv(io.StringIO(sfifty.decode('utf-8')))
-    nifty100 = pd.read_csv(io.StringIO(shundred.decode('utf-8')))
-    nifty200 = pd.read_csv(io.StringIO(stwohundred.decode('utf-8')))
-    nifty500 = pd.read_csv(io.StringIO(sfivehundred.decode('utf-8')))
+    nifty50 = pd.read_csv('fifty.csv')
+    # nifty100 = pd.read_csv(io.StringIO(shundred.decode('utf-8')))
 
     nifty50 = nifty50['Symbol']
-    nifty100 = nifty100['Symbol']
-    nifty200 = nifty200['Symbol']
-    nifty500 = nifty500['Symbol']
+    # nifty100 = nifty100['Symbol']
 
-    stocks = nse.get_stock_codes()
     symbols = []
     raw = []
     price = []
     raw2 = []
 
-    data = [nse.get_quote(stock) for stock in nifty100]
+    data = [nse.get_quote(stock) for stock in nifty50]
     for i in range(len(data)):
         symbols.append([data[i].get('symbol')])
     for x in symbols:
@@ -51,20 +43,9 @@ def index(request):
         price.append([data[j].get('open')])
     for y in price:
         raw2.append(','.join(map(str,y)))
-    data = zip(raw,raw2)
+    data = zip(raw, raw2)
 
-    # symbols.append(data['symbol'])
-    # open = data['open']
-
-
-    context = {
-        'fifty': nifty50,
-        'hundred': nifty100,
-        'twohundred': nifty200,
-        'fivehundred': nifty500,
-        'stocks': data,
-
-               }
+    context = {'stocks': data}
 
     return render(request, 'StockPrediction/index.html', context)
 
